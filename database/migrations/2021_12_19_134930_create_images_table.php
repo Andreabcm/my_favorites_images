2bdd55591ef472f284d298475fd031c0915afa0b
unknown <andreabcm16@gmail.com>
2021-12-19 16:15:34 +0100

    inserting the image and title fields to the images table

--- database/migrations/2021_12_19_134930_create_images_table.php
+++ database/migrations/2021_12_19_134930_create_images_table.php
@@ -17,6 +17,8 @@ class CreateImagesTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('image');
+            $table->string('title', 200);
             $table->timestamps();
         });
     }
