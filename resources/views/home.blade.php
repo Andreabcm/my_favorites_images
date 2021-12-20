3723d236d7976f913a1d97c63d2f665e2ef26cda
unknown <andreabcm16@gmail.com>
2021-12-20 11:03:41 +0100

    printing image url and titleon blade home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<table class="table table-light">
+    
+    <thead class="thead-light">
+        <tr>
+            <th>id</th>
+            <th>Image</th>
+            <th>Title</th>
+        </tr>
+    </thead>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <tbody>
+        @foreach($images as $image)
+        <tr>
+    
+            <td>
+            <a href="{{ route('show', ['id' => $image->id]) }}">
+                <img class="imgCard" src="{{ asset('storage') . '/' . $image->image }}" alt="">
+            </a>
+            </td>
+            
+            <td>{{ $image->image}}</td>
+            <td>{{ $image->title}}</td>
+        
+    
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-<h1>Hello world</h1>
+
 @endsection
